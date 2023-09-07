232def5854ec208b1e3fbba64fd5a6d49844de28
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-09-07 16:54:00 +0300

    Install simpleJWT and implement it

--- library_service/settings.py
+++ library_service/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "rest_framework",
+    "rest_framework_simplejwt",
     "user",
 ]
 
@@ -131,3 +132,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
