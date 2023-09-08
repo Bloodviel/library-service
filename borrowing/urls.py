6f86c20716911d0bf51850d414a06a543cf8b13f
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-09-08 15:29:26 +0300

    Create BorrowingSerializer and create endpoint for borrowings

---
+++ borrowing/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+
+from borrowing.views import BorrowingViewSet
+
+router = DefaultRouter()
+router.register("", BorrowingViewSet)
+
+
+urlpatterns = router.urls
+
+app_name = "borrowing"
