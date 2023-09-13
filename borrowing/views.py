040f1574217d64159f94cc23357fc7136e579384
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-09-13 14:36:16 +0300

    Add filter by params in BorrowingViewSet

--- borrowing/views.py
+++ borrowing/views.py
@@ -14,11 +14,29 @@ class BorrowingViewSet(viewsets.ModelViewSet):
 
         return BorrowingSerializer
 
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
     def get_queryset(self):
         if self.request.user.is_staff:
             queryset = self.queryset
         else:
             queryset = self.queryset.filter(user_id=self.request.user.id)
+
+        user_id = self.request.query_params.get("user_id")
+        is_active = self.request.query_params.get("is_active")
+
+        if user_id:
+            user_ids = self._params_to_ints(user_id)
+            queryset = queryset.filter(user_id__in=user_ids)
+
+        if is_active:
+            if is_active == "true":
+                queryset = queryset.filter(actual_return_date__isnull=True)
+            elif is_active == "false":
+                queryset = queryset.filter(actual_return_date__isnull=False)
 
         return queryset
 
