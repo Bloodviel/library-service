0d2f2dd8da3fa52343945cf55468893c06d59d24
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-09-13 13:28:29 +0300

    Update base BorrowingViewSet and create BorrowingListSerializer

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -12,5 +12,23 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "borrow_date",
             "expected_return_date",
             "book",
+            "is_active"
+        ]
+
+
+class BorrowingListSerializer(serializers.ModelSerializer):
+    book_info = serializers.CharField(read_only=True, source="book")
+    user_full_name = serializers.CharField(read_only=True, source="user")
+
+    class Meta:
+        model = Borrowing
+        fields = [
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "book",
+            "book_info",
             "user",
+            "user_full_name",
+            "is_active"
         ]
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,9 +1,26 @@
 from rest_framework import viewsets
 
 from borrowing.models import Borrowing
-from borrowing.serializers import BorrowingSerializer
+from borrowing.serializers import BorrowingSerializer, BorrowingListSerializer
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.select_related("book", "user")
     serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+
+        return BorrowingSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            queryset = self.queryset
+        else:
+            queryset = self.queryset.filter(user_id=self.request.user.id)
+
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
