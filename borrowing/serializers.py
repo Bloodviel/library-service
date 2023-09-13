97ce4a893b0f351d7462805b6f06563689e5d9c3
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-09-13 14:22:17 +0300

    Make feature when borrow book - inventory should be made -= 1

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
+from book.models import Book
 from borrowing.models import Borrowing
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        data = super(BorrowingSerializer, self).validate(attrs=attrs)
+        book = Book.objects.get(id=attrs["book"].id)
+        book.inventory -= 1
+        if book.inventory >= 0:
+            book.save()
+            return data
+        else:
+            raise ValidationError("Book not available")
 
     class Meta:
         model = Borrowing
