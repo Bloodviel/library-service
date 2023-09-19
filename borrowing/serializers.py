12b5b3c98b49368fa47d98012c23cbba59dc5d5f
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-09-19 17:16:14 +0300

    Update PaymentSerializer, rewrite BorrowingSerializer, create BorrowingReturnSerializer

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,27 +1,30 @@
+from django.utils import timezone
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-from book.models import Book
+from book.serializers import BookSerializer
 from borrowing.models import Borrowing, Payment
 from borrowing.notification_service import send_message
+from borrowing.payment_service import create_stripe_session
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payment
+        fields = [
+            "id",
+            "status",
+            "type",
+            "borrowing",
+            "session_url",
+            "session_id",
+            "money_to_pay"
+        ]
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
-    def validate(self, attrs):
-        data = super(BorrowingSerializer, self).validate(attrs=attrs)
-        book = Book.objects.get(id=attrs["book"].id)
-        book.inventory -= 1
-        if book.inventory >= 0:
-            book.save()
-            message = (
-                f"New Borrowing:\n"
-                f"{book}\n"
-                f"Expected return date: {data['expected_return_date']}"
-            )
-            send_message(message)
-            return data
-        else:
-            raise ValidationError("Book not available")
+    payments = PaymentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Borrowing
@@ -30,11 +33,52 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "borrow_date",
             "expected_return_date",
             "book",
-            "is_active"
+            "is_active",
+            "payments",
         ]
 
+    def validate(self, attrs):
+        user = self.context["request"].user
 
-class BorrowingListSerializer(serializers.ModelSerializer):
+        if Payment.objects.filter(
+            borrowing__user=user,
+            status=Payment.StatusChoices.PENDING
+        ).exists():
+            raise ValidationError(
+                "You cannot borrow a new book."
+            )
+        return attrs
+
+    def validate_book(self, book):
+        if book.inventory == 0:
+            raise ValidationError(
+                "Currently book is not available"
+            )
+        return book
+
+    def create(self, validated_data):
+        request = self.context["request"]
+        book = validated_data["book"]
+        user = request.user
+
+        borrowing = Borrowing.objects.create(
+            expected_return_date=validated_data["expected_return_date"],
+            book=book,
+            user=user
+        )
+        create_stripe_session(request, borrowing)
+        book.inventory -= 1
+        book.save()
+        message = (
+                f"New Borrowing:\n"
+                f"{book}\n"
+                f"Expected return date: {borrowing.expected_return_date}"
+            )
+        send_message(message)
+        return borrowing
+
+
+class BorrowingListSerializer(BorrowingSerializer):
     book_info = serializers.CharField(read_only=True, source="book")
     user_full_name = serializers.CharField(read_only=True, source="user")
 
@@ -71,15 +115,25 @@ class BorrowingDetailSerializer(serializers.ModelSerializer):
         ]
 
 
-class PaymentSerializer(serializers.ModelSerializer):
+class BorrowingReturnSerializer(BorrowingSerializer):
+    expected_return_date = serializers.ReadOnlyField()
+    book = BookSerializer(read_only=True, many=False)
 
-    class Meta:
-        model = Payment
-        fields = [
-            "status",
-            "type",
-            "borrowing",
-            "session_url",
-            "session_id",
-            "money_to_pay"
-        ]
+    def validate(self, attrs):
+        borrowing = self.instance
+
+        if borrowing.actual_return_date is not None:
+            raise ValidationError("This book has been returned")
+
+        return attrs
+
+    def save(self, **kwargs):
+        borrowing = self.instance
+        borrowing.actual_return_date = timezone.now().date()
+        borrowing.save()
+
+        book = borrowing.book
+        book.inventory += 1
+        book.save()
+
+        return borrowing
